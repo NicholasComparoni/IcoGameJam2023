c68d49e653ca4bd1f7d0ef6441bc1e57d2834e7a
Cinfa <cinfa78@gmail.com>
2023-07-31 00:23:25 +0200

    Player Thrusters

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,9 @@ namespace ICO321 {
 	public class PlayerMovement : MonoBehaviour {
 		public float movementSpeed = 5f;
 		public float maxSpeed;
-
+		[SerializeField] private ParticleSystem mainThrusters;
+		[SerializeField] private ParticleSystem[] topThrusters;
+		[SerializeField] private ParticleSystem[] bottomThrusters;
 		private Rigidbody2D rb;
 		private Vector2 direction;
 
@@ -14,6 +16,27 @@ namespace ICO321 {
 
 		private void Update() {
 			direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+
+			if (direction.x == 0) {
+				TurnOffThrusters();
+				mainThrusters.Stop();
+				DoVerticalThrusters();
+			}
+			else if (direction.x > 0) {
+				TurnOffThrusters();
+				mainThrusters.Play();
+				DoVerticalThrusters();
+			}
+			else if (direction.x < 0) {
+				TurnOffThrusters();
+				topThrusters[1].Play();
+				bottomThrusters[1].Play();
+				mainThrusters.Stop();
+				DoVerticalThrusters();
+			}
+
+
 			var position = transform.position;
 
 			if (position.x < BoundariesManager.Instance.bounds.min.x) {
@@ -34,6 +57,39 @@ namespace ICO321 {
 
 		private void FixedUpdate() {
 			rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+		}
+
+		private void TurnOffThrusters() {
+			topThrusters[0].Stop();
+			topThrusters[1].Stop();
+			bottomThrusters[0].Stop();
+			bottomThrusters[1].Stop();
+			
+		}
+		private void DoVerticalThrusters() {
+			if (direction.y > 0) {
+				bottomThrusters[0].Play();
+				bottomThrusters[1].Play();
+			}
+			// else {
+			// 	bottomThrusters[0].Stop();
+			// 	bottomThrusters[1].Stop();
+			// }
+			if (direction.y < 0) {
+				topThrusters[0].Play();
+				topThrusters[1].Play();
+			}
+			// else {
+			// 	topThrusters[0].Stop();
+			// 	topThrusters[1].Stop();
+			// }
+
+			// if (direction.y == 0) {
+			// 	topThrusters[0].Stop();
+			// 	topThrusters[1].Stop();
+			// 	bottomThrusters[0].Stop();
+			// 	bottomThrusters[1].Stop();
+			// }
 		}
 	}
 }
