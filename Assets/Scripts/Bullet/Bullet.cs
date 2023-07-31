8eee499cb8b51e4b69f789bdf9fdc85dc91201e9
Cinfa <cinfa78@gmail.com>
2023-07-31 10:24:04 +0200

    Player bullet sprite

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 namespace ICO321 {
 	public class Bullet : MonoBehaviour {
 		[SerializeField] private SpriteRenderer spriteRenderer;
+		//[SerializeField] private ParticleVfx particleVfx;
+		[SerializeField] private Sprite[] bulletSprites;
 		[SerializeField] private TypesUtility.Phase phase;
 		[SerializeField] private Vector3 direction;
 		[SerializeField] private GameObject hitVfx;
 		[SerializeField] private AudioClip hitClip;
+
+		private int currentSprite;
 
 		private Collider2D col2D;
 		public float speed;
@@ -20,6 +24,7 @@ namespace ICO321 {
 			set {
 				phase = value;
 				spriteRenderer.color = PhaseManager.Instance.GetPhaseColor();
+				//particleVfx.Color = PhaseManager.Instance.GetPhaseColor();
 			}
 		}
 
@@ -38,6 +43,8 @@ namespace ICO321 {
 			float angleDeg = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.Euler(0f, 0f, angleDeg);
 			transform.position += Direction * (speed * Time.deltaTime);
+			spriteRenderer.sprite = bulletSprites[currentSprite];
+			currentSprite = (currentSprite + 1) % bulletSprites.Length;
 		}
 
 		private void OnTriggerEnter2D(Collider2D other) {
@@ -53,7 +60,7 @@ namespace ICO321 {
 			vfx.transform.position = transform.position;
 			vfx.GetComponent<ParticleVfx>().Color = spriteRenderer.color;
 			vfx.GetComponent<ParticleVfx>().Play();
-			SfxManager.Instance.PlayClip(hitClip,0.2f);
+			SfxManager.Instance.PlayClip(hitClip, 0.2f);
 			gameObject.SetActive(false);
 		}
 	}
